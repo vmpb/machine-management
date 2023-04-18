103da57fe8ea61bff3dbd2dd60274c8d1575d86c
Vincent <vbrangoulo@gmail.com>
2023-04-18 16:33:34 +0200

    searchbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { AppBar, Toolbar, IconButton, Typography, Button } from '@mui/material';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+  InputBase,
+  Box,
+} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
 import { useNavigate } from 'react-router-dom';
 
-
-
 function Navbar({ onHamburgerClick, isLoggedIn }) {
-
   const navigate = useNavigate();
 
   const handleLogout = () => {
@@ -17,7 +23,7 @@ function Navbar({ onHamburgerClick, isLoggedIn }) {
   return (
     <AppBar position="static">
       <Toolbar>
-      {isLoggedIn() && (
+        {isLoggedIn() && (
           <IconButton
             edge="start"
             color="inherit"
@@ -27,11 +33,50 @@ function Navbar({ onHamburgerClick, isLoggedIn }) {
             <MenuIcon />
           </IconButton>
         )}
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Typography
+          variant="h6"
+          noWrap
+          component="div"
+          sx={{
+            flexGrow: 1,
+            display: 'flex',
+            justifyContent: 'flex-start',
+          }}
+        >
           Concurrent
         </Typography>
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            borderRadius: 1,
+            pl: 0.5,
+            pr: 0.5,
+            flexGrow: 1,
+            justifyContent: 'center',
+          }}
+        >
+          <SearchIcon />
+          <InputBase
+            placeholder="Search…"
+            inputProps={{ 'aria-label': 'search' }}
+            sx={{
+              color: 'inherit',
+              ml: 1,
+            }}
+            disableUnderline
+          />
+        </Box>
         {isLoggedIn() && (
-          <Button color="inherit" onClick={handleLogout}>
+          <Button
+            color="inherit"
+            onClick={handleLogout}
+            sx={{
+              flexGrow: 1,
+              display: 'flex',
+              justifyContent: 'flex-end',
+            }}
+          >
             Logout
           </Button>
         )}
